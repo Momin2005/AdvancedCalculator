1ecca2da9bc353c40ac5cbeeba9d53e06652a0cd
Momin <maelouahabi>
2021-01-24 18:47:50 +0100

    parametres

---
+++ parameters.py
@@ -0,0 +1,38 @@
+import math
+
+class Parameters():
+     def plus(self, x, y):
+          return x + y
+
+     def min(self, x, y):
+          return x - y
+
+     def multiply(self, x, y):
+          return x * y
+
+     def divide(self, x, y):
+          return x/y
+
+     def power(self, x, y):
+          return x ** y
+
+     def square(self, x):
+          return math.sqrt(x)
+
+     def sinus(self, x):
+         return math.sin(x)
+
+     def cosinus(self, x):
+          return math.cos(x)
+
+     def tangens(self, x):
+          return math.tan(x)
+
+     def cosecans(self, x):
+          return math.sinh(x)
+
+     def secans(self, x):
+          return math.cosh(x)
+     
+     def cotangens(self, x):
+          return math.tanh(x)
