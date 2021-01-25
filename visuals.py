548b50ae0604270d753ff6d5ce1875e6be0733ee
Bebenk15243 <pepijn.simoens@gmail.com>
2021-01-25 16:20:02 +0100

    begonnen aan de buttons in "visuals.py" en de input beginnen maken vr de getalle en de opperators.

---
+++ visuals.py
@@ -0,0 +1,24 @@
+from settings import *
+
+class vistual_setup:
+    def __init__(self):
+        self.font = '("new courier", 20)'
+        self.squire = 5
+
+    def input_disp(self, number, disp):
+        backup = disp.get()
+        disp.delete(0, END)
+        disp.insert(0, backup, number)
+        return disp
+
+    def entry(self):
+        disp = Entry(win, columnspan=1)
+        disp.grid(row=0, column=0)
+        return disp
+
+    def init_numeric_buttons(self):
+        button1 = Button(win, text="1", font=self.font, width=self.squire, padx=self.squire, pady=self.squire,command=lambda:input_disp(1, disp))
+        button1.grid(row=0, column=3)
+
+
+
